468e3495e91aebc97c0303a694619069cd34caf1
mwlk <mirkoivowlk@gmail.com>
2025-08-24 21:15:41 -0300

    add carts route's implementation

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const config = require("./configs/config.js");
 
 const productsRoutes = require("./src/routes/products.routes.js");
+const cartsRoutes = require("./src/routes/carts.routes.js");
 
 const app = express();
 const PORT = config.PORT;
@@ -9,11 +10,13 @@ const PORT = config.PORT;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
+//! default route for check
+app.get("/", (_, res) => {
   res.status(200).send("Welcome to the Product API");
 });
 
 app.use("/api/products", productsRoutes);
+app.use("/api/carts", cartsRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
